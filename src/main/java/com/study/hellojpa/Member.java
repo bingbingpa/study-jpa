2bf59e0bc0d41a460d6e6a6ded231fc6567bcbca
bingbingpa <guriguri1576@gmail.com>
2019-10-29 00:41:40 +0900

    jpa @Entity 스터디, ddl 자동 생성으로 옵션 변경

--- src/main/java/com/study/hellojpa/Member.java
+++ src/main/java/com/study/hellojpa/Member.java
@@ -6,6 +6,14 @@ import javax.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * @Entity은 클래스는 JPA가 관리한다.
+ * JPA를 사용해서 테이블과 매핑할 클래스는 @Entity가 필수.
+ * 기본 생성자는 필수 
+ * final 클래스, enum, interface, inner 클래스 사용 X 
+ * 저장할 필드에 final 사용 X
+ * @Table은 엔티티와 매피할 테이블 지정
+ */
 @Entity
 @Getter
 @Setter
