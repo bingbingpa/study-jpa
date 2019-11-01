a939506f04936882ed12d8c16ff2f6fd72b63265
shpark <guriguri1576@gmail.com>
2019-11-02 01:41:46 +0900

    누락 부분 커밋

--- jpa-shop/src/main/java/com/bingbingpa/jpashop/DemoApplication.java
+++ jpa-shop/src/main/java/com/bingbingpa/jpashop/DemoApplication.java
@@ -11,6 +11,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		/**
+		 * 데이터 중심 설계의 문제점
+		 * 현재 방식은 객체 설계를 테이블 설계에 맞춘 방식
+		 * 테이블의 외래키를 객체에 그대로 가져옴 
+		 * 객체 그래프 탐색이 불가능 
+		 * 참조가 없으므로 UML도 잘못됨
+		 */
 		// SpringApplication.run(DemoApplication.class, args);
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 		EntityManager em = emf.createEntityManager();
