1dc118f1cdcb55abda295a76b515af5ac6baea60
shpark <bingbingpa@gmail.com>
2021-02-05 00:13:08 +0900

    EntityManager crud, jpql 맛보기

--- README.md
+++ README.md
@@ -17,3 +17,10 @@
 - JPA 설정파일은 /META-INF/persistence.xml 
     - hibernate.dialect: 어떤 데이터베이스 인지 지정. ProgressDialect, H2Dialect, MySQL5InnoDBDialect 등등... 40가지 이상의 데이터베이스 방언 지원
     - javax 로 시작하는 옵션들은 자바 표준이고 hibernate 로 시작하는 것들은 hibernate 에서만 쓰이는 옵션들이다. 다른 orm 구현체를 쓸 경우 javax 옵션이 아닌 경우는 바뀔 수 있다.
+    - hibernate.show_sql: 쿼리를 보여준다.
+    - hibernate.format_sql: 쿼리를 정렬해서 보여준다.
+    - hibernate.use_sql_comments: 어떤 쿼리인지 코멘트를 보여준다.
+- 엔티티 매니저 팩토리는 하나만 생성해서 애플리케이션 전체에서 공유
+- **엔티티 매니저는 쓰레드간에 절대로 공유하면 안된다.**
+- **JPA 의 모든 데이터 변경은 트랜잭션 안에서 실행해야 한다.**
+- JPQL: SQL 을 추상화한 **객체 지향 쿼리 언어**
---
+++ ex1-hello-jpa/src/main/java/me.bingbingpa/JpaMain.java
@@ -0,0 +1,37 @@
+package me.bingbingpa;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+//            Member member = new Member();
+//            member.setId(2L);
+//            member.setName("HelloB");
+//            Member findMember = em.find(Member.class, 1L);
+//            findMember.setName("HelloJPA");
+//            System.out.println("findMember.id ===== " + findMember.getId());
+//            System.out.println("findMember.name ===== " + findMember.getName());
+//            em.persist(member);
+            List<Member> result = em.createQuery("select m from Member as m", Member.class).getResultList();
+            for (Member member : result) {
+                System.out.println("member ==== " + member.getName());
+            }
+            tx.commit();
+        } catch (Exception e) {
+            em.close();
+        }
+
+
+        emf.close();
+    }
+}
