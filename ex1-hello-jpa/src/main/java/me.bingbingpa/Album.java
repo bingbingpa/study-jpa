51088545a5f4972965b13b86fb8cceeaf8c405ba
shpark <bingbingpa@gmail.com>
2021-03-08 20:22:30 +0900

    상속 관계 매핑

---
+++ ex1-hello-jpa/src/main/java/me.bingbingpa/Album.java
@@ -0,0 +1,11 @@
+package me.bingbingpa;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("A")
+public class Album extends Item {
+
+    private String artist;
+}
--- ex1-hello-jpa/src/main/java/me.bingbingpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/me.bingbingpa/JpaMain.java
@@ -4,7 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 public class JpaMain {
 
@@ -17,27 +16,19 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Movie movie = new Movie();
+            movie.setDirector("aaaa");
+            movie.setActor("bbbb");
+            movie.setName("바람과 함께 사라지다");
+            movie.setPrice(10000);
 
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setTeam(team);
-            em.persist(member);
+            em.persist(movie);
 
             em.flush();
             em.clear();
 
-            Member findMember = em.find(Member.class, member.getId());
-
-            Team findTeam = findMember.getTeam();
-            System.out.println("findTeam ===== " + findTeam.getName());
-
-            List<Member> members = findMember.getTeam().getMembers();
-            for (Member m : members) {
-                System.out.println("m =========== " + m.getUsername());
-            }
+            Movie findMove = em.find(Movie.class, movie.getId());
+            System.out.println("findMove ================ " + findMove);
 
             tx.commit();
         } catch (Exception e) {
